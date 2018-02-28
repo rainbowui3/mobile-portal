9074674dd4676fb009554c76478d281ff8a2ee7f
xinwei.wu <493795648@qq.com>
2018-02-28 18:32:52 +0800

    add

---
+++ src/pages/project/components/HolderInfo.vue
@@ -0,0 +1,44 @@
+<<template>
+  <card>
+      <r-input :title="$t('投保人姓名:')" :placeholder="$t('请填写投保人姓名')" :model="policy" value="value2" />
+      <selector  :title="$t('投保人证件类型:')" :options="options" :model="certificate" value="certificateId" :onChange="onChange"></selector>
+      <r-input :title="$t('投保人证件号码:')" :placeholder="$t('请填写证件号码')" :model="policy" value="value2" />
+      <date-time  :title="$t('投保人出生日期')" :model="policy" value="nullDate" :required="true" :onChange="onChange"></date-time>
+      <r-input :title="$t('投保人手机号码:')" :placeholder="$t('请填写手机号码')" :model="policy" value="value2" />   
+      <r-input :title="$t('投保人电子邮箱：')" :placeholder="$t('email@email.com')" :model="policy" value="value2" :isEmail="true" :validate="false"/>
+  </card>
+</template>
+
+<<script>
+import { Page, Card, RInput, RButton,Selector,DateTime } from "rainbow-mobile-core";
+export default {
+    components:{
+        Card,
+        RInput,
+        Selector,
+        DateTime
+
+    },
+    data() {
+        return{
+            policy:{
+                value2:''
+            },
+            options:[{"key":"10000","value":"省份证"},{"key":"10001","value":"军官证"},{"key":"10002","value":"护照"}],
+            certificate:{
+                "certificateId":"10000",
+            }
+
+        };
+    },
+    method:{
+        onChange:function(val) {
+        this.certificate.effortDate=val;
+        }
+    }
+  
+}
+</script>
+<<style>
+
+</style>
