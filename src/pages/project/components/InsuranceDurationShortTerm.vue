aa040f956bb4d503783abbe4887e7630f7f3f561
zhangbl <zhangbl50@outlook.com>
2018-03-02 10:10:17 +0800

    修改组件，最外层套div

--- src/pages/project/components/InsuranceDurationShortTerm.vue
+++ src/pages/project/components/InsuranceDurationShortTerm.vue
@@ -2,8 +2,8 @@
 <template>
     <div>
         <card>
-            <date-time :title="$t('EffectiveDate')" :model="policy" value="effectiveDate" :format="timeFormat"></date-time>
-            <date-time :title="$t('ExpireDate')" :model="policy" value="expireDate" :format="timeFormat"></date-time>
+        <date-time :title="$t('EffectiveDate')" :model="policy" value="effectiveDate" :format="timeFormat"></date-time>
+        <date-time :title="$t('ExpireDate')" :model="policy" value="expireDate" :format="timeFormat"></date-time>
         </card>
     </div>
 </template>
@@ -12,27 +12,27 @@
 import { Card, DateTime } from "rainbow-mobile-core";
 import config from "config";
 export default {
-    components: {
-        DateTime,
-        Card,
+  components: {
+    DateTime,
+    Card
+  },
+  data() {
+    return {
+      policy: {
+        effectiveDate: "",
+        expireDate: ""
+      },
+      timeFormat: config.DEFAULT_DATETIME_FORMATER
+    };
+  },
+  methods: {
+    onChange(val) {
+      this.policy.effortDate = val;
     },
-    data() {
-        return {
-            policy: {
-                effectiveDate: "",
-                expireDate: ""
-            },
-            timeFormat: config.DEFAULT_DATETIME_FORMATER
-        };
-    },
-    methods: {
-        onChange(val) {
-            this.policy.effortDate = val;
-        },
-        VOnChange: function(event) {
-            console.log(event);
-        }
+    VOnChange: function(event) {
+      console.log(event);
     }
+  }
 };
 </script>
 <i18n> 
--- src/pages/project/components/PlanSelection.vue
+++ src/pages/project/components/PlanSelection.vue
@@ -1,6 +1,7 @@
 //方案选择组件
 <template>
-    <card>
+    <div>
+        <!-- <card> -->
         <tab :tabItems="tabItems" />
         <swiper height="400px" :model="index" onChange=this.onIndexChange>
             <swiper-item v-for="(item,idx) in items" :key="idx">
@@ -12,132 +13,133 @@
                 </card>
             </swiper-item>
         </swiper>
-    </card>
+        <!-- </card> -->
+    </div>
 </template>
 <script>
 import {
-    Card,
-    Tab,
-    Swiper,
-    SwiperItem,
-    RTable,
-    List
+  Card,
+  Tab,
+  Swiper,
+  SwiperItem,
+  RTable,
+  List
 } from "rainbow-mobile-core";
 import { PassThrough } from "stream";
 import { debug } from "util";
 export default {
-    components: {
-        Tab,
-        Card,
-        Swiper,
-        SwiperItem,
-        RTable,
-        List
-    },
-    data: function() {
-        return {
-            items: this.getSwiperItems(),
-            index: 0,
-            planData: {
-                head: [[{ text: "保险责任" }, { text: "保险金额" }]],
-                body: [
-                    [{ text: "1" }, { text: "1" }],
-                    [{ text: "2" }, { text: "2" }],
-                    [{ text: "3" }, { text: "3" }]
-                ]
-            }
-        };
-    },
-    computed: {
-        //Tab选项卡的名称
-        tabItems: function() {
-            return [
-                {
-                    selected: true,
-                    showdot: false,
-                    disabled: false,
-                    bage: "",
-                    text: "item1",
-                    onClick: this.onTabItemClicked
-                },
-                {
-                    selected: false,
-                    showdot: false,
-                    disabled: false,
-                    bage: "",
-                    text: "item2",
-                    onClick: this.onTabItemClicked
-                },
-                {
-                    selected: false,
-                    showdot: false,
-                    disabled: false,
-                    bage: "",
-                    text: "item3",
-                    onClick: this.onTabItemClicked
-                }
-            ];
+  components: {
+    Tab,
+    Card,
+    Swiper,
+    SwiperItem,
+    RTable,
+    List
+  },
+  data: function() {
+    return {
+      items: this.getSwiperItems(),
+      index: 0,
+      planData: {
+        head: [[{ text: "保险责任" }, { text: "保险金额" }]],
+        body: [
+          [{ text: "1" }, { text: "1" }],
+          [{ text: "2" }, { text: "2" }],
+          [{ text: "3" }, { text: "3" }]
+        ]
+      }
+    };
+  },
+  computed: {
+    //Tab选项卡的名称
+    tabItems: function() {
+      return [
+        {
+          selected: true,
+          showdot: false,
+          disabled: false,
+          bage: "",
+          text: "item1",
+          onClick: this.onTabItemClicked
         },
-        getListData() {
-            return [
-                { label: this.$t("适用人群:"), value: "XXXXXX" },
-                { label: this.$t("每人限购:"), value: "XXXXX份" },
-                { label: this.$t("保险期限:"), value: "XXXXX年" },
-                { label: this.$t("特殊说明:"), value: "XXXXXXXXX" }
-            ];
+        {
+          selected: false,
+          showdot: false,
+          disabled: false,
+          bage: "",
+          text: "item2",
+          onClick: this.onTabItemClicked
+        },
+        {
+          selected: false,
+          showdot: false,
+          disabled: false,
+          bage: "",
+          text: "item3",
+          onClick: this.onTabItemClicked
         }
+      ];
     },
-    methods: {
-        onIndexChange:function(value){
-            console.log(value);
-            debugger;
-        },
-        getSwiperItems: function() {
-            return [
-                {
-                    planData: {
-                        head: [[{ text: "保险责任" }, { text: "保险金额" }]],
-                        body: [
-                            [{ text: "11" }, { text: "11" }],
-                            [{ text: "12" }, { text: "12" }],
-                            [{ text: "13" }, { text: "13" }]
-                        ]
-                    }
-                },
-                {
-                    planData: {
-                        head: [[{ text: "保险责任" }, { text: "保险金额" }]],
-                        body: [
-                            [{ text: "21" }, { text: "21" }],
-                            [{ text: "22" }, { text: "22" }],
-                            [{ text: "23" }, { text: "23" }]
-                        ]
-                    }
-                },
-                {
-                    planData: {
-                        head: [[{ text: "保险责任" }, { text: "保险金额" }]],
-                        body: [
-                            [{ text: "31" }, { text: "31" }],
-                            [{ text: "32" }, { text: "32" }],
-                            [{ text: "33" }, { text: "33" }]
-                        ]
-                    }
-                }
-            ];
-        },
-        onTabItemClicked: function(index) {
-            this.index = index;
-        }
-    },
-    mounted: function() {
-        console.log(this.items);
+    getListData() {
+      return [
+        { label: this.$t("适用人群:"), value: "XXXXXX" },
+        { label: this.$t("每人限购:"), value: "XXXXX份" },
+        { label: this.$t("保险期限:"), value: "XXXXX年" },
+        { label: this.$t("特殊说明:"), value: "XXXXXXXXX" }
+      ];
     }
+  },
+  methods: {
+    onIndexChange: function(value) {
+      console.log(value);
+      debugger;
+    },
+    getSwiperItems: function() {
+      return [
+        {
+          planData: {
+            head: [[{ text: "保险责任" }, { text: "保险金额" }]],
+            body: [
+              [{ text: "11" }, { text: "11" }],
+              [{ text: "12" }, { text: "12" }],
+              [{ text: "13" }, { text: "13" }]
+            ]
+          }
+        },
+        {
+          planData: {
+            head: [[{ text: "保险责任" }, { text: "保险金额" }]],
+            body: [
+              [{ text: "21" }, { text: "21" }],
+              [{ text: "22" }, { text: "22" }],
+              [{ text: "23" }, { text: "23" }]
+            ]
+          }
+        },
+        {
+          planData: {
+            head: [[{ text: "保险责任" }, { text: "保险金额" }]],
+            body: [
+              [{ text: "31" }, { text: "31" }],
+              [{ text: "32" }, { text: "32" }],
+              [{ text: "33" }, { text: "33" }]
+            ]
+          }
+        }
+      ];
+    },
+    onTabItemClicked: function(index) {
+      this.index = index;
+    }
+  },
+  mounted: function() {
+    console.log(this.items);
+  }
 };
 </script>
 <style>
 .weui-panel__hd {
-    text-align: left;
+  text-align: left;
 }
 </style>
 <i18n>
