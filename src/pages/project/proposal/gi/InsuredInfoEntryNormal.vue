60a831b0dd4118af105ed29bd1136570d1c4794a
zhangbl <zhangbl50@outlook.com>
2018-03-05 11:17:57 +0800

    proposalConfirm组件增加点击事件，页面跳转传值

--- src/pages/project/proposal/gi/InsuredInfoEntryNormal.vue
+++ src/pages/project/proposal/gi/InsuredInfoEntryNormal.vue
@@ -1,24 +1,51 @@
 <template>
     <page>
         <top />
-        <holder-info/>
-        
+        <insurance-duration-short-term/>
+        <card :title="$t('投保人信息')">
+            <holder-info/>
+        </card>
+        <card :title="$t('被保险人信息')">
+            <insured-info/>
+        </card>
+        <card :title="$t('附属被保险人信息')">
+            <subsidiary-insured-info/>
+        </card>
+        <proposal-clause-confirm/>
+        <card>
+            <proposal-confirm-imme/>
+        </card>
     </page>
 </template>
 
 <script>
-import {Page} from "rainbow-mobile-core";
+import { Page, Card } from "rainbow-mobile-core";
 import Top from "@/components/Top";
 import Bottom from "@/components/Bottom";
-import HolderInfo from "../../components/HolderInfo"
-
+import HolderInfo from "../../components/HolderInfo";
+import InsuranceDurationShortTerm from "../../components/InsuranceDurationShortTerm";
+import InsuredInfo from "../../components/InsuredInfo";
+import SubsidiaryInsuredInfo from "../../components/SubsidiaryInsuredInfo";
+import ProposalClauseConfirm from "../../components/ProposalClauseConfirm";
+import ProposalConfirmImme from "../../components/ProposalConfirmImme";
 export default {
-    components: {
-        Top,
-        Bottom,
-        Page,
-        HolderInfo,
-    }
+  components: {
+    Top,
+    Card,
+    Bottom,
+    Page,
+    HolderInfo,
+    InsuranceDurationShortTerm,
+    InsuredInfo,
+    SubsidiaryInsuredInfo,
+    ProposalClauseConfirm,
+    ProposalConfirmImme
+  },
+  mounted:function(){
+      this.$on("productInfoEntryNormalBtn",function(a){
+          console.log(a);
+      });
+  }
 };
 </script>
 
--- src/pages/project/proposal/gi/ProductInfoEntryNormal.vue
+++ src/pages/project/proposal/gi/ProductInfoEntryNormal.vue
@@ -1,29 +1,43 @@
 <template>
-   <page>
-       <top :title="$t('project.jtyw')" :showBack="true" />
-       <card>
-           <product-top></product-top>
-       </card>
-       <card>
-           <insurance-duration-currency></insurance-duration-currency>
-       </card>
-       <card>
-           <plan-selection></plan-selection>
-       </card>
-       <card>
-           <proposal-copies></proposal-copies>
-       </card>
-       <tab-bar>
-           <proposal-confirm></proposal-confirm>
-       </tab-bar>
+    <page>
+        <top :title="$t('project.jtyw')" :showBack="true" />
+        <card>
+            <product-top></product-top>
+        </card>
+        <card>
+            <insurance-duration-currency></insurance-duration-currency>
+        </card>
+        <card>
+            <plan-selection></plan-selection>
+        </card>
+        <card>
+            <proposal-copies></proposal-copies>
+        </card>
+        <tab-bar>
+            <proposal-confirm :onClick="onClick"></proposal-confirm>
+        </tab-bar>
 
-   </page>
+    </page>
 
-  
 </template>
 
 <script>
-import {Page,Card,RImage,RButton,Swiper,RSwitch,DateTime,Tab,SwiperItem,RTable,List,Selector,TabBar,Cell} from 'rainbow-mobile-core';
+import {
+  Page,
+  Card,
+  RImage,
+  RButton,
+  Swiper,
+  RSwitch,
+  DateTime,
+  Tab,
+  SwiperItem,
+  RTable,
+  List,
+  Selector,
+  TabBar,
+  Cell
+} from "rainbow-mobile-core";
 import Top from "../../../../components/Top";
 import Bottom from "../../../../components/Bottom";
 import ProductTop from "../../components/ProductTop";
@@ -32,20 +46,35 @@ import PlanSelection from "../../components/PlanSelection";
 import ProposalCopies from "../../components/ProposalCopies";
 import ProposalConfirm from "../../components/ProposalConfirm";
 export default {
-    components:{
-        Page,
-        Card,
-        TabBar,
-        Top,
-        Bottom,
-        ProductTop,
-        InsuranceDurationCurrency,
-        PlanSelection,
-        ProposalCopies,
-        ProposalConfirm
+  components: {
+    Page,
+    Card,
+    TabBar,
+    Top,
+    Bottom,
+    ProductTop,
+    InsuranceDurationCurrency,
+    PlanSelection,
+    ProposalCopies,
+    ProposalConfirm
+  },
+  data() {
+    return {
+      policy: {
+        data1: 1,
+        data2: 2
+      }
+    };
+  },
+  methods: {
+    onClick: function() {
+        debugger;
+      console.log(this.policy);
+      this.$emit("productInfoEntryNormalBtn", this.policy);
+      this.$router.push("/project/proposal/gi/insuredInfoEntryNormal");
     }
-  
-}
+  }
+};
 </script>
 
 <style>
