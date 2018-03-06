274d9379f9ec3a70f7694430449c3262094e3fd1
zhangbl <zhangbl50@outlook.com>
2018-03-06 15:23:28 +0800

    修改top

--- src/pages/project/proposal/gi/ProductInfoEntryShortTerm.vue
+++ src/pages/project/proposal/gi/ProductInfoEntryShortTerm.vue
@@ -1,29 +1,46 @@
 <template>
-   <page>
-       <top :title="$t('project.jtyw')" :showBack="true" />
-       <card>
-           <product-top></product-top>
-       </card>
-       <card>
-           <insurance-duration-shortTerm></insurance-duration-shortTerm>
-       </card>
-       <card>
-           <plan-selection></plan-selection>
-       </card>
-       <card>
-           <proposal-copies></proposal-copies>
-       </card>
-       <tab-bar>
-           <proposal-confirm v-bind:linkInsuredInfoUrl="linkInsuredInfoUrl" :onClick="onClick"></proposal-confirm>
-       </tab-bar>
+    <page>
+        <top :title="$t('project.jtyw')" :showBack="true" />
+        <r-body>
+            <card>
+                <product-top></product-top>
+            </card>
+            <card>
+                <insurance-duration-shortTerm></insurance-duration-shortTerm>
+            </card>
+            <card>
+                <plan-selection></plan-selection>
+            </card>
+            <card>
+                <proposal-copies></proposal-copies>
+            </card>
+        </r-body>
+        <tab-bar>
+            <proposal-confirm v-bind:linkInsuredInfoUrl="linkInsuredInfoUrl" :onClick="onClick"></proposal-confirm>
+        </tab-bar>
 
-   </page>
+    </page>
 
-  
 </template>
 
 <script>
-import {Page,Card,RImage,RButton,Swiper,RSwitch,DateTime,Tab,SwiperItem,RTable,List,Selector,TabBar,Cell} from 'rainbow-mobile-core';
+import {
+  Page,
+  Card,
+  RImage,
+  RButton,
+  Swiper,
+  RSwitch,
+  DateTime,
+  Tab,
+  SwiperItem,
+  RTable,
+  List,
+  Selector,
+  TabBar,
+  Cell,
+  RBody
+} from "rainbow-mobile-core";
 import Top from "../../../../components/Top";
 import Bottom from "../../../../components/Bottom";
 import ProductTop from "../../components/ProductTop";
@@ -34,37 +51,36 @@ import ProposalConfirm from "../../components/ProposalConfirm";
 // import localStorage from "../../../../sotre.js";
 // import policyJson from "../../../../policy.json";
 export default {
-    components:{
-        Page,
-        Card,
-        TabBar,
-        Top,
-        Bottom,
-        ProductTop,
-        InsuranceDurationShortTerm,
-        PlanSelection,
-        ProposalCopies,
-        ProposalConfirm
-    },
-    data() {
-        return{
-            linkInsuredInfoUrl:"/project/proposal/gi/insuredInfoEntryShortTerm"
-            // policy:Json.stringify(policyJson)
-            
-        };
-    },
-    methods:{
-        onClick:function(){
-            this.$router.push("/project/proposal/gi/insuredInfoEntryShortTerm");
-        }
+  components: {
+    Page,
+    Card,
+    TabBar,
+    Top,
+    Bottom,
+    ProductTop,
+    InsuranceDurationShortTerm,
+    PlanSelection,
+    ProposalCopies,
+    ProposalConfirm,
+    RBody
+  },
+  data() {
+    return {
+      linkInsuredInfoUrl: "/project/proposal/gi/insuredInfoEntryShortTerm"
+      // policy:Json.stringify(policyJson)
+    };
+  },
+  methods: {
+    onClick: function() {
+      this.$router.push("/project/proposal/gi/insuredInfoEntryShortTerm");
     }
-    // ,
-    // mounted:function(){
+  }
+  // ,
+  // mounted:function(){
 
-    //     console.log(this.policy)
-    // }
-  
-}
+  //     console.log(this.policy)
+  // }
+};
 </script>
 
 <style>
