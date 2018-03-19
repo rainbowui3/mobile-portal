8393bb93bb4fc8f6bb543b2d4c562b01e6246a2e
narifu@163.com <narifu@163.com>
2018-03-19 09:49:32 +0800

    update

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-import messages from './messages';
+import common from './common';
 import {zh_CN,en_US} from 'rainbow-mobile-core'
 Vue.use(VueI18n);
-_.merge(messages["zh-CN"], zh_CN);
-_.merge(messages["en-US"], en_US);
+_.merge(common["zh-CN"], zh_CN);
+_.merge(common["en-US"], en_US);
 const i18n = new VueI18n({
     locale: "zh-CN", 
-    messages, 
+    common, 
 })
 export default i18n;
