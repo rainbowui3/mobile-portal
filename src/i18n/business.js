bb6650a5a3561f6dc24db91da9acecc92f3c5e75
zhangbl <zhangbl50@outlook.com>
2018-03-19 19:01:36 +0800

    国际化：将common中的提出变成小js文件

---
+++ src/i18n/business.js
@@ -0,0 +1,22 @@
+import i18n from "./index";
+i18n.mergeLocaleMessage("zh-CN", {
+    "business.holderInfo": "投保人",
+    "business.insuranceDurationCurrency": "保险期",
+    "business.insuredInfo": "被保人",
+    "business.planSelection": "方案",
+    "business.productTop": "产品",
+    "business.proposalClauseConfirm": "条款",
+    "business.proposalCopies": "保费",
+    "business.pay": "支付",
+    "business.payment": "确认支付"
+});
+i18n.mergeLocaleMessage("en-US", {
+    "business.holderInfo": "policyHolder",
+    "business.insuranceDurationCurrency": "insuranceData",
+    "business.insuredInfo": "insured",
+    "business.planSelection": "plan",
+    "business.productTop": "product",
+    "business.proposalClauseConfirm": "Item",
+    "business.proposalCopies": "promine",
+    "business.pay": "pay",
+});
