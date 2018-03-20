98f5dcad84016064feaa4b7fbc6334dab2b3c4c5
zhangbl <zhangbl50@outlook.com>
2018-03-20 11:04:18 +0800

    PlanSelection国际化

--- src/i18n/planSelection.js
+++ src/i18n/planSelection.js
@@ -2,10 +2,18 @@ import i18n from "./index";
 i18n.mergeLocaleMessage("zh-CN",{ 
     "planSelection.guaranteeRate": "保障范围",
     "planSelection.notes": "投保须知",
-    "planSelection.plan":"方案" 
+    "planSelection.plan":"方案" ,
+    "planSelection.approvalCrowd":"适用人群",
+    "planSelection.limitPurchase":"每人限购",
+    "planSelection.term":"保险期限",
+    "planSelection.specialInstructions":"特别说明"
 });
 i18n.mergeLocaleMessage("en-US",{
     "planSelection.guaranteeRate": "Scope of protection",
     "planSelection.notes": "Notes",
-    "planSelection.plan":"Plan"
+    "planSelection.plan":"Plan",
+    "planSelection.approvalCrowd":"Approval Crowd",
+    "planSelection.limitPurchase":"Limit Purchase",
+    "planSelection.term":"Term",
+    "planSelection.specialInstructions":"Special Instructions"
 });
