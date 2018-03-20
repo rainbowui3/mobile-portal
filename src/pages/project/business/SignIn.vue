9cf96011511597cbc8b223ba15efdb56cc639238
yueping.bai <yueping.bai@yuepingbaideMacBook-Air.local>
2018-03-20 18:32:17 +0800

    updata

--- src/pages/project/business/SignIn.vue
+++ src/pages/project/business/SignIn.vue
@@ -1,19 +1,25 @@
 <template>
-   <!-- <card :title="$t('signin in')">
-
-   </card> -->
+   <r-image :list="images"/>
 </template>
 
 <script>
-import { Card } from "rainbow-mobile-core";
+import { Card ,RImage } from "rainbow-mobile-core";
+import Dialog from "../../../assets/dialog.jpg";
+
 export default {
   components: {
+    RImage,
     Card
   },
   data() {
     return {
-     
+     images:[
+          // {"class":"image","src":Ly,'onClick':this.goto,'param':'/project/ly'},
+     ]
     };
   },
+  methods:{
+      
+  }
 };
 </script>
