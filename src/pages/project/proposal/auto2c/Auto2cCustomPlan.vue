06462cf3f989dbfb206d06465c3f223938a381e9
xinwei.wu <493795648@qq.com>
2018-04-03 15:21:55 +0800

    add

--- src/pages/project/proposal/auto2c/Auto2cCustomPlan.vue
+++ src/pages/project/proposal/auto2c/Auto2cCustomPlan.vue
@@ -3,19 +3,65 @@
         <top :title="$t('common.autoInsurance')" :showBack="true" />
         <r-body>
             <card>
-                <r-switch  :title="$t('auto2cCustomPlan.compulsoryInsurance')"  :model="policy" value="isComp" ></r-switch>
+                <r-switch  :title="$t('auto2cCustomPlan.compulsoryInsurance')" :model="policy" value="isComp" ></r-switch>
                 <div v-show="policy.isComp">                    
                 <date-time :title="$t('auto2cCustomPlan.compStart')" :model="policy" value="compEffectiveDate" :format="hoursFormat" ></date-time>                  
                 </div>        
             </card>
             <card>
-                <date-time :title="$t('auto2cCustomPlan.commStart')" :model="policy" value="commEffectiveDate" :format="hoursFormat" ></date-time>                                
-                <row :title="$t('auto2cCustomPlan.vehicleLoss')" :model="policy" value="value" link="/project/proposal/auto2e/VehicleLoss"></row>
-                <row :title="$t('auto2cCustomPlan.thirdDutyMian')" :model="policy" value="value" link="/project/proposal/auto2e/ThirdDutyMian"></row>
+                <date-time :title="$t('auto2cCustomPlan.commStart')" :model="policy" value="" :format="hoursFormat" ></date-time>                                
+                <!--<row-and-checker :rcTitle="$t('auto2cCustomPlan.vehicleLoss')" :model="" :linkUrl=""></row-and-checker>-->
+                <cell :type="row" class="margin_brage">
+                    <cell  :padding="padding" :span="4">
+                        <div v-text="$t('auto2cCustomPlan.vehicleLoss')" />
+                    </cell>
+                    <cell  :padding="padding" :span="4">
+                        <checker :model="policy" value="vehicleLossSDEW" :text="$t('auto2cCustomPlan.sdew')" type="icon"></checker>                     
+                    </cell>
+                    <cell  :padding="padding" class="padding_brage" >
+                        <row :model="policy" value="value"  link="/project/proposal/auto2e/VehicleLoss"></row>
+                    </cell>
+                </cell>
+                
+                <!--<row :title="$t('auto2cCustomPlan.vehicleLoss')" :model="policy" value="value" link="/project/proposal/auto2e/VehicleLoss"></row>-->
+                <cell :type="row" class="margin_brage">
+                    <cell  :padding="padding" :span="4">
+                        <div v-text="$t('auto2cCustomPlan.thirdDutyMian')" />
+                    </cell>
+                    <cell  :padding="padding" :span="4">
+                        <checker :model="policy" value="thirdDutyMianSDEW" :text="$t('auto2cCustomPlan.sdew')" type="icon"></checker>                     
+                    </cell>
+                    <cell  :padding="padding" class="padding_brage" >
+                        <row :model="policy" value="value"  link="/project/proposal/auto2e/ThirdDutyMian"></row>
+                    </cell>
+                </cell>
+                <!--<row :title="$t('auto2cCustomPlan.thirdDutyMian')" :model="policy" value="value" link="/project/proposal/auto2e/ThirdDutyMian"></row>-->
                 <r-switch  :title="$t('auto2cCustomPlan.carRobberyMain')"  :model="policy" value="carRobberyMain" ></r-switch>
                 <!--<row :title="$t('auto2cCustomPlan.carRobberyMain')" :model="policy" value="value" link="/project/proposal/auto2e/CarRobberyMain"></row>-->
-                <row :title="$t('auto2cCustomPlan.driverDutyMian')" :model="policy" value="value" link="/project/proposal/auto2e/DriverDutyMian"></row>
-                <row :title="$t('auto2cCustomPlan.passengerDutyMian')" :model="policy" value="value" link="/project/proposal/auto2e/PassengerDutyMian"></row>
+                <cell :type="row" class="margin_brage">
+                    <cell  :padding="padding" :span="4">
+                        <div v-text="$t('auto2cCustomPlan.driverDutyMian')" />
+                    </cell>
+                    <cell  :padding="padding" :span="4">
+                        <checker :model="policy" value="driverDutyMianSDEW" :text="$t('auto2cCustomPlan.sdew')" type="icon"></checker>                     
+                    </cell>
+                    <cell  :padding="padding" class="padding_brage" >
+                        <row :model="policy" value="value"  link="/project/proposal/auto2e/DriverDutyMian"></row>
+                    </cell>
+                </cell>
+                <!--<row :title="$t('auto2cCustomPlan.driverDutyMian')" :model="policy" value="value" link="/project/proposal/auto2e/DriverDutyMian"></row>-->
+                <cell :type="row" class="margin_brage">
+                    <cell  :padding="padding" :span="4">
+                        <div v-text="$t('auto2cCustomPlan.passengerDutyMian')" />
+                    </cell>
+                    <cell  :padding="padding" :span="4">
+                        <checker :model="policy" value="passengerDutyMianSDEW" :text="$t('auto2cCustomPlan.sdew')" type="icon"></checker>                     
+                    </cell>
+                    <cell  :padding="padding" class="padding_brage" >
+                        <row :model="policy" value="value"  link="/project/proposal/auto2e/passengerDutyMian"></row>
+                    </cell>
+                </cell>
+                <!--<row :title="$t('auto2cCustomPlan.passengerDutyMian')" :model="policy" value="value" link="/project/proposal/auto2e/PassengerDutyMian"></row>-->
                 <r-switch  :title="$t('auto2cCustomPlan.glassCrushAdditional')"  :model="policy" value="glassCrushAdditional" ></r-switch>
                 <!--<row :title="$t('auto2cCustomPlan.glassCrushAdditional')" :model="policy" value="value" link="/project/proposal/auto2e/GlassCrushAdditional"></row>-->
                 <r-switch  :title="$t('auto2cCustomPlan.naturalDamage')"  :model="policy" value="naturalDamage" ></r-switch>
@@ -32,12 +78,9 @@
                  <!--<row :title="$t('auto2cCustomPlan.notFoundThirdAdditional')" :model="policy" value="value"  :isLink="true"></row>-->
                 <r-switch  :title="$t('auto2cCustomPlan.appointRepairShop')"  :model="policy" value="appointRepairShop" ></r-switch>
                 <!--<row :title="$t('auto2cCustomPlan.appointRepairShop')" :model="policy" value="value" link="/project/proposal/auto2e/AppointRepairShop"></row>-->
-               <r-input :title="$t('auto2cCustomPlan.appointRepairShop')"  :model="policy" value="appointRepairShop" :middle="true">
-                    <span class="fa fa-bicycle fa-2x"/>
-                </r-input>
             </card>
             <div class="button_vertical-align">
-               <r-button type="default" :mini="true" :onClick="calculatePremium">{{$t('auto2cCustomPlan.reset')}}</r-button>                        
+               <r-button type="default" :mini="true" :onClick="resetClick">{{$t('auto2cCustomPlan.reset')}}</r-button>                        
             </div>
 
         </r-body>
@@ -83,14 +126,14 @@ export default {
     Cell,
     Checker,
     Row,
-    DateTime
+    DateTime,
   },
   data() {
     return {
         hoursFormat:config.DEFAULT_HOURS_FORMATER,
         row: "row",
+        padding:'0px',
         policy: {
-            appointRepairShop:"",
             commEffectiveDate:"",
             compEffectiveDate:"",
             carRobberyMain:false,
@@ -105,7 +148,11 @@ export default {
             isComm:true,
             value2:"",
             value:"不投保",
-            discount:"0.7"
+            discount:"0.7",
+            passengerDutyMianSDEW:false,
+            driverDutyMianSDEW:false,
+            thirdDutyMianSDEW:false,
+            vehicleLossSDEW:true,
 
         },
         proCodeOptions:[],
@@ -117,14 +164,12 @@ export default {
   methods: {
     calculatePremium(){      
         this.$router.push("/project/proposal/auto2e/AutoPremiumInfo");
-        // this.$router.push("");
+    },
+    resetClick(){
 
     },
     onChange(){
 
-    },
-    gotoVehicleAndVesse(event){
-      window.location.hash="/project/proposal/auto2e/AutoVehicleAndVesselTax";
     },
     goto(event){
       // window.location.hash="/project/proposal/auto2e/VehicleLoss";
@@ -139,9 +184,17 @@ export default {
 
 <style>
 .button_vertical-align {
-    margin-top: 10px;
-    align-content: center
+    margin-top: 10px;  
+    text-align: center;
 }
+.margin_brage{
+    margin-left: 15px;
+    margin-right: 30px;
+ }
+.padding_brage{
+    
+    padding-right: 16px;
+ }
     
 
 </style>
