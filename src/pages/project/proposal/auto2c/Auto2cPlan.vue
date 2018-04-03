5612534393b5b3a62eebbefed6f2e5d3d3871175
zhangbl <zhangbl50@outlook.com>
2018-04-03 09:24:47 +0800

    Auto2cPlan

--- src/pages/project/proposal/auto2c/Auto2cPlan.vue
+++ src/pages/project/proposal/auto2c/Auto2cPlan.vue
@@ -2,11 +2,22 @@
     <page>
         <top :showBack="true" :title="$t('auto2cPlan.title')" />
         <r-body>
-            <tab :tabItems="tabItems" class="tab"/>
+            <tab :tabItems="tabItems" class="tab" />
             <swiper :model="index">
                 <swiper-item>
                     <card>
-                        <div></div>
+                        <row title="fdsafsafdsa" :model="pageModel" />
+                        <div>
+                            <div class="planName">
+                                {{"xxxxx险"}}
+                            </div>
+                            <div class="planAddition">
+                                {{"不计免赔"}}
+                            </div>
+                            <div class="planAmount">
+                                {{"15000元"}}
+                            </div>
+                        </div>
                     </card>
                 </swiper-item>
                 <swiper-item>
@@ -33,7 +44,8 @@ import {
   Tab,
   Swiper,
   SwiperItem,
-  RButton
+  RButton,
+  Row
 } from "rainbow-mobile-core";
 import Top from "../../../../components/Top";
 import "../../../../i18n/auto2cPlan";
@@ -48,36 +60,76 @@ export default {
     Swiper,
     SwiperItem,
     RButton,
-    Top
+    Top,
+    Row
   },
   data() {
     return {
-        index:0
+      index: 0,
+      pageModel: {}
     };
   },
   computed: {
     tabItems() {
       return [
-          {'selected':true, 'showdot':false, 'disabled':false,'badge':'','text':this.$t('auto2cPlan.hotSale'),"onClick":this.goto},
-          {'selected':false, 'showdot':false, 'disabled':false,'badge':'','text':this.$t('auto2cPlan.basic'),"onClick":this.goto},
-          {'selected':false, 'showdot':false, 'disabled':false,'badge':'','text':this.$t('auto2cPlan.advance'),"onClick":this.goto},
-        ];
+        {
+          selected: true,
+          showdot: false,
+          disabled: false,
+          badge: "",
+          text: this.$t("auto2cPlan.hotSale"),
+          onClick: this.goto
+        },
+        {
+          selected: false,
+          showdot: false,
+          disabled: false,
+          badge: "",
+          text: this.$t("auto2cPlan.basic"),
+          onClick: this.goto
+        },
+        {
+          selected: false,
+          showdot: false,
+          disabled: false,
+          badge: "",
+          text: this.$t("auto2cPlan.advance"),
+          onClick: this.goto
+        }
+      ];
     }
   },
-  methods:{
-      goto:function(index){
-          console.log(index);
-          this.index = index;
-      }
+  methods: {
+    goto: function(index) {
+      console.log(index);
+      this.index = index;
+    }
   }
 };
 </script>
 
 <style>
-.tab > div{
-    height: 100px;
+.tab > div {
+  height: 100px;
 }
-.tab>.vux-tab>.vux-tab-item{
-    line-height: 100px;
+.tab > .vux-tab > .vux-tab-item {
+  line-height: 100px;
+}
+.planName {
+  float: left;
+  font-size: 17px;
+  padding: 10px 5px 10px 15px;
+}
+.planAddition {
+  float: left;
+  font-size: 13px;
+  margin: 15px 0 15px 0;
+  background: blue;
+  color: white;
+}
+.planAmount {
+  float: right;
+  font-size: 17px;
+  padding: 10px 15px;
 }
 </style>
