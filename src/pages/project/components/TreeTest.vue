e7c3b093053cba5085127e4e72c99b773f07f9af
zhangbl <zhangbl50@outlook.com>
2018-04-08 14:08:40 +0800

    HealthInform.vue

---
+++ src/pages/project/components/TreeTest.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(data,idx) in dataList" :key="idx">
+                <r-switch :model="data" value="selected" :title="data.name"/>
+                <tree-test v-if="data.hasChild == true" :dataList="data.child" class="a"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { RSwitch } from "rainbow-mobile-core";
+import TreeTest from "./TreeTest";
+export default {
+  name: "treeTest",
+  components: { RSwitch, TreeTest },
+  props: {
+    dataList: {
+      type: Array
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.a{
+    padding-left: 20px;
+}
+
+ul{
+    list-style:none;
+}
+</style>
