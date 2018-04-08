1d9c3ea1775aff46100127e836ca006ac5801caf
yueping.bai <yueping.bai@yuepingbaideMacBook-Air.local>
2018-04-08 14:43:28 +0800

    updata

--- src/i18n/businessHome.js
+++
@@ -1,9 +0,0 @@
-import i18n from "./index";
-i18n.mergeLocaleMessage("zh-CN", {
-    "businessHome.title": "自定义标题",
-    "businessHome.home": "首页"
-});
-i18n.mergeLocaleMessage("en-US", {
-    "businessHome.title": "title",
-    "businessHome.home": "home"
-});
