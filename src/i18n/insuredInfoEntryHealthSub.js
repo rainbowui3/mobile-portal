067367ac1d6fc889531411e4640e5caddd2c691a
zhangbl <zhangbl50@outlook.com>
2018-04-08 14:02:37 +0800

    ChooseRelationship、ProposalClauseConfrim、InsuredInfoHealthSub

---
+++ src/i18n/insuredInfoEntryHealthSub.js
@@ -0,0 +1,11 @@
+import i18n from "./index";
+i18n.mergeLocaleMessage("zh-CN",{
+    "insuredInfoEntryHealthSub.title":"投保信息录入(含附属被保险人)",
+    "insuredInfoEntryHealthSub.toast":"请阅读并确认各服务条款",
+    "insuredInfoEntryHealthSub.healthInfo":"健康告知",
+});
+i18n.mergeLocaleMessage("en-US",{
+    "insuredInfoEntryHealthSub.title":"InsuredInfoEntryHealthSub",
+    "insuredInfoEntryHealthSub.toast":"Please read and confirm clauses",
+    "insuredInfoEntryHealthSub.healthInfo":"Health Info"
+});
---
+++ src/pages/project/proposal/ah/InsuredInfoConfirmHealthSub.vue
@@ -0,0 +1,143 @@
+<template>
+    <page>
+        <top :title="$t('insuredInfoEntryHealthSub.title')" :showBack="true" />
+        <r-body>
+            <!-- 保险期限选择 -->
+            <card>
+                <insurance-duration-short-term type="day" :model="policy" effectiveDate="effectiveDate" expireDate="expireDate" :readonly="true" />
+            </card>
+            <!-- 投保人信息 -->
+            <card :title="$t('common.holderInfo')">
+                <holder-info :holderInfo="policy.holderInfo" :readonly="true" />
+            </card>
+            <!-- 被保人信息 -->
+            <card :title="$t('common.insuredInfo')">
+                <choose-relationship :datas="datas1" :model="policy.insuredInfo" value="relationToHolder" :title="$t('holderInfo.relationToHolder')" :readonly="true" />
+                <insured-info v-if="policy.insuredInfo.relationToHolder != '1'" :insuredInfo="policy.insuredInfo" :readonly="true" />
+                <row :model="pageModel" :title="$t('insuredInfoEntryHealthSub.healthInfo')" :isLink="true" :onClick="gotoHealthInfo" />
+            </card>
+            <!-- 附属被保险人信息 -->
+            <card :title="$t('common.subsidiaryInsuredInfo')">
+                <choose-relationship :datas="datas1" :model="policy.subsidiaryInfo" value="relationToHolder" :title="$t('holderInfo.relationToHolder')" :readonly="true" />
+                <choose-relationship :datas="datas1" :model="policy.subsidiaryInfo" value="relationToMainInsured" :title="$t('holderInfo.relationToInsured')" :readonly="true" />
+                <subsidiary-insured-info v-if="policy.subsidiaryInfo.relationToHolder != '1'" :dubsidiaryInsuranceInfo="policy.subsidiaryInfo" :readonly="true" />
+                <row :model="pageModel" :title="$t('insuredInfoEntryHealthSub.healthInfo')" :isLink="true" :onClick="gotoHealthInfo" />
+            </card>
+        </r-body>
+        <tab-bar>
+            <proposal-confirm amount="50" :buttonName="'proposalConfirm.submitPay'" :onClick="goto" />
+        </tab-bar>
+    </page>
+</template>
+
+<script>
+import {
+  Page,
+  Card,
+  TabBar,
+  RBody,
+  RButton,
+  Cell,
+  Selector,
+  RInput,
+  Row,
+  Toast
+} from "rainbow-mobile-core";
+import Top from "../../../../components/Top";
+import ChooseRelationship from "../../components/ChooseRelationship";
+import ProposalConfirm from "../../components/ProposalConfirm";
+import HolderInfo from "../../components/HolderInfo";
+import InsuredInfo from "../../components/InsuredInfo";
+import SubsidiaryInsuredInfo from "../../components/SubsidiaryInsuredInfo";
+import ProposalClauseConfirm from "../../components/ProposalClauseConfirm";
+import InsuranceDurationShortTerm from "../../components/InsuranceDurationShortTerm";
+import "../../../../i18n/insuredInfoEntryHealthSub";
+import "../../../../i18n/proposalConfirm";
+import "../../../../i18n/holderInfo";
+
+export default {
+  components: {
+    Page,
+    Card,
+    TabBar,
+    RBody,
+    RButton,
+    Cell,
+    Selector,
+    RInput,
+    Row,
+    Top,
+    ChooseRelationship,
+    ProposalConfirm,
+    HolderInfo,
+    InsuredInfo,
+    SubsidiaryInsuredInfo,
+    ProposalClauseConfirm,
+    InsuranceDurationShortTerm,
+    Toast
+  },
+  data() {
+    return {
+      policy: {
+        holderInfo: {},
+        insuredInfo: {
+          relationToHolder: "1"
+        },
+        subsidiaryInfo: {
+          relationToHolder: "1"
+        }
+      },
+      pageModel: {
+        clauseConfirmed: false,
+        toastShow: false
+      },
+      datas1: [
+        {
+          key: "1",
+          value: "本人",
+          active: true
+          // onClick: this.onClickInsured
+        },
+        {
+          key: "2",
+          value: "配偶",
+          active: false
+          // onClick: this.onClickInsured
+        },
+        {
+          key: "3",
+          value: "子女",
+          active: false
+          // onClick: this.onClickInsured
+        },
+        {
+          key: "4",
+          value: "父母",
+          active: false
+          // onClick: this.onClickInsured
+        }
+      ]
+    };
+  },
+  methods: {
+    goto: function() {
+      //Todo:跳转到下一个页面,去支付
+      console.log("gotoPay");
+    },
+    gotoHealthInfo: function() {
+      console.log("gotoHealthInfo");
+    }
+  },
+  watch: {},
+  computed: {},
+  created: function() {
+    if (this.$route.params.policy) {
+      this.policy = this.$route.params.policy;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
