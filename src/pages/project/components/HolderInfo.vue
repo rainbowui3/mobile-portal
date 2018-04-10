69a4d29dff4d43671337da6e390e728905a61f91
zhangbl <zhangbl50@outlook.com>
2018-04-10 16:32:28 +0800

    修复bug

--- src/pages/project/components/HolderInfo.vue
+++ src/pages/project/components/HolderInfo.vue
@@ -2,7 +2,8 @@
   <div>
     <r-input :title="$t('holderInfo.name')" :placeholder="$t('holderInfo.placeholderName')" :model="model" value="name" :readonly="readonly" />
     <selector :title="$t('holderInfo.certificateType')" :options="options" :model="model" value="certificateId" :onChange="onChangeCertiType" :readonly="readonly"></selector>
-    <r-input :title="$t('holderInfo.ID')" :placeholder="$t('holderInfo.placeholderID')" :model="model" value="certificateNum" :validator="validateNumInput" :novalidate="isValidateNum" :readonly="readonly" />
+    <r-input v-if="isValidateNum == true" :title="$t('holderInfo.ID')" :placeholder="$t('holderInfo.placeholderID')" :model="model" value="certificateNum" :validator="validateNumInput" :novalidate="false" :readonly="readonly" />
+    <r-input v-else-if="isValidateNum == false" :placeholder="$t('holderInfo.placeholderID')" :title="$t('holderInfo.ID')" :model="model" value="certificateNum" :readonly="readonly"/>
     <date-time :title="$t('holderInfo.birthday')" :model="model" value="birthdate" :required="true" :readonly="readonly"></date-time>
     <r-input :title="$t('holderInfo.mobile')" :placeholder="$t('holderInfo.placeholderMobile')" :model="model" value="mobileNum" :isPhone="true" :novalidate="false" :readonly="readonly" />
     <r-input :title="$t('holderInfo.email')" :placeholder="$t('holderInfo.placeholderEmail')" :model="model" value="email" :isEmail="true" :readonly="readonly" :novalidate="false" />
@@ -35,7 +36,7 @@ export default {
         { key: "10000", value: "身份证" },
         { key: "10002", value: "护照" }
       ],
-      isValidateNum: false
+      isValidateNum: true
     };
   },
   methods: {
@@ -48,22 +49,21 @@ export default {
         valid: isCertification === true,
         msg: this.$t("input.validate")
       };
+    },
+    onChangeCertiType: function() {
+      if (this.model.certificateId && this.model.certificateId === "10000") {
+        this.isValidateNum = true;
+        this.validateNumInput(this.model.certificateNum);
+      } else {
+        this.isValidateNum = false;
+      }
     }
   },
   props: {
     readonly: Boolean,
     model: Object
   },
-  computed: {
-    onChangeCertiType: function() {
-      if (this.model.certificateId && this.model.certificateId === "10000") {
-        this.isValidateNum = false;
-        this.validateNumInput(this.model.certificateNum);
-      } else {
-        this.isValidateNum = true;
-      }
-    }
-  }
+  computed: {}
 };
 </script>
 <style>
--- src/pages/project/components/InsuredInfo.vue
+++ src/pages/project/components/InsuredInfo.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        <!-- <selector  :title="$t('与投保人关系:')" :options="options1" :model="model" value="relationToHolder" :onChange="onChange" :readonly="readonly"></selector> -->
-        <r-input :title="$t('holderInfo.name')" :placeholder="$t('holderInfo.placeholderName')" :model="model" value="name" :readonly="readonly" />
-        <selector :title="$t('holderInfo.certificateType')" :options="options" :model="model" value="certificateId" :onChange="onChangeCertiType" :readonly="readonly"></selector>
-        <r-input :title="$t('holderInfo.ID')" :placeholder="$t('holderInfo.placeholderID')" :model="model" value="certificateNum" :validator="validateNumInput" :novalidate="isValidateNum" :readonly="readonly" />
-        <date-time :title="$t('holderInfo.birthday')" :model="model" value="birthdate" :required="true" :onChange="onChange" :readonly="readonly"></date-time>
-        <r-input :title="$t('holderInfo.mobile')" :placeholder="$t('holderInfo.placeholderMobile')" :model="model" value="mobileNum" :isPhone="true" :novalidate="false" :readonly="readonly" />
-        <r-input :title="$t('holderInfo.email')" :placeholder="$t('holderInfo.placeholderEmail')" :model="model" value="email" :isEmail="true" :readonly="readonly" :novalidate="false" />
-    </div>
+  <div>
+    <!-- <selector  :title="$t('与投保人关系:')" :options="options1" :model="model" value="relationToHolder" :onChange="onChange" :readonly="readonly"></selector> -->
+    <r-input :title="$t('holderInfo.name')" :placeholder="$t('holderInfo.placeholderName')" :model="model" value="name" :readonly="readonly" />
+    <selector :title="$t('holderInfo.certificateType')" :options="options" :model="model" value="certificateId" :onChange="onChangeCertiType" :readonly="readonly"></selector>
+    <r-input v-if="isValidateNum == true" :title="$t('holderInfo.ID')" :placeholder="$t('holderInfo.placeholderID')" :model="model" value="certificateNum" :validator="validateNumInput" :novalidate="false" :readonly="readonly" />
+    <r-input v-else-if="isValidateNum == false" :placeholder="$t('holderInfo.placeholderID')" :title="$t('holderInfo.ID')" :model="model" value="certificateNum" :readonly="readonly" />
+    <date-time :title="$t('holderInfo.birthday')" :model="model" value="birthdate" :required="true" :onChange="onChange" :readonly="readonly"></date-time>
+    <r-input :title="$t('holderInfo.mobile')" :placeholder="$t('holderInfo.placeholderMobile')" :model="model" value="mobileNum" :isPhone="true" :novalidate="false" :readonly="readonly" />
+    <r-input :title="$t('holderInfo.email')" :placeholder="$t('holderInfo.placeholderEmail')" :model="model" value="email" :isEmail="true" :readonly="readonly" :novalidate="false" />
+  </div>
 </template>
 
 <script>
@@ -30,11 +31,12 @@ export default {
     DateTime
   },
   props: {
-    readonly:Boolean,
-    model:Object
+    readonly: Boolean,
+    model: Object
   },
   data() {
     return {
+      isValidateNum:true,
       policy: {
         value2: ""
       },
@@ -69,18 +71,16 @@ export default {
   },
   computed: {
     onChangeCertiType: function() {
-      if (
-        this.model.certificateId &&
-        this.model.certificateId === "10000"
-      ) {
-        this.isValidateNum = false;
+      if (this.model.certificateId && this.model.certificateId === "10000") {
+        this.isValidateNum = true;
         this.validateNumInput(this.model.certificateNum);
       } else {
-        this.isValidateNum = true;
+        this.isValidateNum = false;
       }
     }
   }
 };
 </script>
 <style>
+
 </style>
