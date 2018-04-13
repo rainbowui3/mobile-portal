12d074c036cdfea1348259953df90af8c3890d00
zhangbl <zhangbl50@outlook.com>
2018-04-13 11:16:14 +0800

    add路由

--- src/pages/project/proposal/ah/AHRouterProduct.vue
+++ src/pages/project/proposal/ah/AHRouterProduct.vue
@@ -11,7 +11,8 @@
 export default {
   data() {
     return {
-      value:""
+      value: "",
+      typeList:[]
     };
   },
   created: function() {
@@ -42,11 +43,16 @@ export default {
       case "shortTerm":
         {
           this.$router.push({
-            path: "/project/proposal/ah/productInfoEntryShortTerm",
-            name: "ProductInfoEntryShortTerm",
+            path:
+              "/proposal/ah/AHRouterProduct/" +
+              this.$route.params.productCode +
+              "/" +
+              this.$route.params.agentCode +
+              "/" +
+              "shortTerm",
+            name: "AHRouterProductShortTerm",
             params: {
-              productCode: this.$route.params.productCode,
-              agentCode: this.$route.params.agentCode
+
             }
           });
         }
@@ -86,27 +92,48 @@ export default {
     //Todo:读配置表，获取路径等信息
 
     //mock:
-    next((vm) => {
-      switch(vm.$route.params.productCode){
-        case '1':{
-          vm.value="normal";
-        }break;
-        case '2':{
-          vm.value="shortTerm";
-        }break;
-        case '3':{
-          vm.value="shortTerm";
-        }break;
-        case '4':{
-          vm.value="normal"
-        }break;
-        case '5':{
-          vm.value="normal";
-        }break;
-        case '6':{
-          vm.value="health"
-        }break;
-        default:{}break;
+    next(vm => {
+      switch (vm.$route.params.productCode) {
+        case "1":
+          {
+            vm.value = "normal";
+            vm.typeList = ['normal','normal','normal','normal'];
+          }
+          break;
+        case "2":
+          {
+            vm.value = "shortTerm";
+            vm.typeList = ['shortTerm','shortTerm','normal','normal'];
+          }
+          break;
+        case "3":
+          {
+            vm.value = "shortTerm";
+            vm.typeList = ['shortTerm','risk','risk','normal'];
+          }
+          break;
+        case "4":
+          {
+            vm.value = "normal";
+            vm.typeList = ['normal','passenger','passenger','normal'];
+          }
+          break;
+        case "5":
+          {
+            vm.value = "normal";
+            vm.typeList = ['normal','studyRisk','studyRisk','normal'];
+          }
+          break;
+        case "6":
+          {
+            vm.value = "health";
+            vm.typeList = ['health','health','health','normal']
+          }
+          break;
+        default:
+          {
+          }
+          break;
       }
     });
   },
