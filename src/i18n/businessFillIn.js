664d080fe75c1d4140573ef07ad86ee9e457a8de
yueping.bai <yueping.bai@yuepingbaideMacBook-Air.local>
2018-04-16 16:14:11 +0800

    updata

--- src/i18n/businessFillIn.js
+++ src/i18n/businessFillIn.js
@@ -1,19 +1,3 @@
-import i18n from "./index";
-i18n.mergeLocaleMessage("zh-CN", {
-    "businessFillIn.title": '保单填写',
-    "businessFillIn.confirm":'立即投保',
-    "businessFillIn.holderInfo":"投保人信息",
-    "businessFillIn.insuredInfo":"被保人信息"
-
-      
-
-});
-i18n.mergeLocaleMessage("en-US", {
-    "businessFillIn.title": '保单填写',
-    "businessFillIn.confirm":'立即投保',
-    "businessFillIn.holderInfo":"投保人信息",
-    "businessFillIn.insuredInfo":"被保人信息"
-});
 import i18n from './index';
 i18n.mergeLocaleMessage('zh-CN', {
     'businessFillIn.title': '保单填写',
