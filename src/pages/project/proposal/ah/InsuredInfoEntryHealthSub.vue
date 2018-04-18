cd24ae0286a90512ca5d7fa3aa43183ce9273436
zhangbl <zhangbl50@outlook.com>
2018-04-18 11:33:00 +0800

    modify

--- src/pages/project/proposal/ah/InsuredInfoEntryHealthSub.vue
+++ src/pages/project/proposal/ah/InsuredInfoEntryHealthSub.vue
@@ -56,7 +56,7 @@ export default {
     InsuredInfo,
     SubsidiaryInsuredInfo,
     ProposalClauseConfirm,
-    InsuranceDurationShortTerm,
+    InsuranceDurationShortTerm
   },
   data() {
     return {
