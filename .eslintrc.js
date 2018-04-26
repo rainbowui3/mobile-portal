34dcbae6a77481c5eb817093829530970443f8f7
tony <tony@tonydeMacBook-Pro.local>
2018-04-26 18:53:17 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   "globals": {
     "sessionStorage": true,
     "alert": true,
-    "AjaxUtil":true
+    "AjaxUtil":true,
+    "_":true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
